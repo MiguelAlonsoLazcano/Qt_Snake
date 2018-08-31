ca8beda1ae362c63dace296aa22791d5cfb25f12
Miguel Angel <alonso.lazcano.ma@gmail.com>
2018-08-31 19:41:12 -0400

    Added project main window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
+#include <QString>
 
-int main() {
-    
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    /*
+     * @brief construct a QApplication before using a QWidget
+     * */
+    QApplication a(argc,argv);
+    QMainWindow window;
+    QLabel *label = new QLabel(&window);
+    QString qt_version = "Qt version: ";
+    qt_version.append(QT_VERSION_STR);
+    window.resize(300,300);
+    window.setWindowTitle("Qt Snake");
+    label->setText(qt_version);
+    window.show();
+
+    return a.exec();
 
 }
